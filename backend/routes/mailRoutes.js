e4761f0e374c46acc12116f3852a9cc120bf3a90
Tatsuya28 <ioiodauphin880@gmail.com>
2025-03-08 16:16:36 -0500

    Addition of new routes for mail management: retrieval of a mail by ID, retrieval of mails by user, and updating the status of a mail

--- backend/routes/mailRoutes.js
+++ backend/routes/mailRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const mailController = require('../controllers/mailController');
 
 router.get('/mails', mailController.getMails);
+router.get('/mails/:id', mailController.getMailById);
+router.get('/mails/user/:userId', mailController.getMailsByUserId);
+router.put('/mails/:id', mailController.updateMailStatus);
 
 module.exports = router;
