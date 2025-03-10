168694a2b5f6ac1e04de7f06a00f791dc8e24a88
Tatsuya28 <ioiodauphin880@gmail.com>
2025-03-10 17:06:08 -0400

    Add vue-toastification for toast notifications in the application

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import './assets/main.css'
 import 'primeicons/primeicons.css'
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +11,24 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+
+const toastOptions = {
+  position: "top-right",
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 20,
+  newestOnTop: true,
+}
+app.use(Toast, toastOptions)
 
 app.mount('#app')
