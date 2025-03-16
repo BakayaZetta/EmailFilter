0cc9e23f5c309e388fb9f6bfd23415d26189b2d3
Tatsuya28 <ioiodauphin880@gmail.com>
2025-03-16 15:16:44 -0400

    feat: Add FooterComponent and restructure App.vue layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/NavBarComponent.vue';
+import Footer from '@/components/FooterComponent.vue';
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
+  <div class="flex flex-col min-h-screen">
+    <NavBar />
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
 </template>
